c508e1dd0ed62e981cb2a9e2f686558955df1781
Stefan Wansch <stefan-wansch@gmx.at>
2018-12-05 17:39:53 +0100

    Finished Homework

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
-import {CREATED, BAD_REQUEST, UNAUTHORIZED} from 'http-status-codes';
 import * as loki from 'lokijs';
+import { CREATED, BAD_REQUEST, UNAUTHORIZED } from 'http-status-codes';
+
+
 import * as express from 'express';
 import * as basic from 'express-basic-auth';
 
-var app = express();
+
+let titleP = "Birthday Party";
+let locationP = "At school";
+let dateP = new Date(2018, 6, 25);
+let port = 8000;
+let app = express();
 app.use(express.json());
 
-const adminFilter = basic({ users: { admin: 'P@ssw0rd!' }});
+app.get('/party', (request, response, next) => {
+    response.send({
+        title: titleP,
+        location: locationP,
+        date: dateP
+    });
+});
 
 const db = new loki(__dirname + '/db.dat', {autosave: true, autoload: true});
 let guests = db.getCollection('guests');
@@ -14,30 +27,33 @@ if (!guests) {
   guests = db.addCollection('guests');
 }
 
-app.get('/guests', adminFilter, (req, res) => {
-  res.send(guests.find());
+app.get('/guests',(request, response, next)=>{
+    /*for(let i=0;i<guests.count();i++){
+
+    }*/
+    response.send(guests.find());
 });
 
-app.get('/party', (req, res, next) => {
-  res.send({
-    title: 'Happy new year!',
-    location: 'At my home',
-    date: new Date(2017, 0, 1)
-  });
-});
-
-app.post('/register', (req, res, next) => {
-  if (!req.body.firstName || !req.body.lastName) {
-    res.status(BAD_REQUEST).send('Missing mandatory member(s)');
-  } else {
-    const count = guests.count();
-    if (count < 10) {
-      const newDoc = guests.insert({firstName: req.body.firstName, lastName: req.body.lastName});
-      res.status(CREATED).send(newDoc);
-    } else {
-      res.status(UNAUTHORIZED).send('Sorry, max. number of guests already reached');
+app.post('/register', function(request,response){
+    if(request.body.firstName==false || request.body.lastName==false){
+        response.status(BAD_REQUEST).send("Falsche Informationen wurden übergeben!");
+    }else{
+        if(guests.count()>10){
+            response.status(UNAUTHORIZED).send("Die maximale Anzahl an Besuchern ist bereits erreicht!");
+        }else{
+            let guest= {
+                firstName: request.body.firstName,
+                lastName: request.body.lastName
+            }
+            let inserted=false;
+            inserted=guests.insert(guest);
+            if(inserted){
+                response.status(CREATED).send("Erfolgreich hinzugefügt!");
+            }else{
+                response.send("Etwas ist schief gelaufen!");
+            }
+        }
     }
-  }
 });
+app.listen(port, () => console.log('API is listening on '+port));
 
-app.listen(8080, () => console.log('API is listening'));
